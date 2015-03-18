e93a1b5ab4f0fa43d0a816db4e27a747c622d819
Bastien <gbastien2@hotmail.fr>
2015-03-18 16:06:58 +0100

    ajout datepicker

--- app/src/main/java/com/dev/callofbeer/fragments/PanelFragment.java
+++ app/src/main/java/com/dev/callofbeer/fragments/PanelFragment.java
@@ -7,23 +7,66 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.fourmob.datetimepicker.date.DatePickerDialog;
+import com.fourmob.datetimepicker.date.DatePickerDialog.OnDateSetListener;
+import com.sleepbot.datetimepicker.time.RadialPickerLayout;
+import com.sleepbot.datetimepicker.time.TimePickerDialog;
 
 import com.dev.callofbeer.R;
-import com.getbase.floatingactionbutton.FloatingActionButton;
-import com.getbase.floatingactionbutton.FloatingActionsMenu;
+
+import java.util.Calendar;
 
 /**
  * Created by matth on 04/03/15.
  */
-public class PanelFragment extends Fragment {
+public class PanelFragment extends Fragment implements OnDateSetListener, TimePickerDialog.OnTimeSetListener{
 
-
+    public static final String DATEPICKER_TAG = "datepicker";
+    public static final String TIMEPICKER_TAG = "timepicker";
+    public Button butDate, butTime;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.view_panel, container, false);
+        View view = inflater.inflate(R.layout.view_create_event, container, false);
+
+        final Calendar calendar = Calendar.getInstance();
+        final DatePickerDialog datePickerDialog = DatePickerDialog.newInstance(this, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), true);
+        final TimePickerDialog timePickerDialog = TimePickerDialog.newInstance(this, calendar.get(Calendar.HOUR_OF_DAY) ,calendar.get(Calendar.MINUTE), false, false);
+        butDate = (Button) view.findViewById(R.id.dateButton);
+        butDate.setText(calendar.get(Calendar.DAY_OF_MONTH)+"/"+calendar.get(Calendar.MONTH)+"/"+calendar.get(Calendar.YEAR));
+        butDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePickerDialog.setVibrate(true);
+                datePickerDialog.setYearRange(1985, 2028);
+                datePickerDialog.setCloseOnSingleTapDay(true);
+                datePickerDialog.show(getActivity().getSupportFragmentManager(), DATEPICKER_TAG);
+            }
+        });
+        butTime = (Button) view.findViewById(R.id.timeButton);
+        butTime.setText(calendar.get(Calendar.HOUR_OF_DAY) + "H" + String.format("%02d", calendar.get(Calendar.MINUTE)));
+        butTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timePickerDialog.setVibrate(true);
+                timePickerDialog.setCloseOnSingleTapMinute(true);
+                timePickerDialog.show(getActivity().getSupportFragmentManager(), TIMEPICKER_TAG);
+            }
+        });
 
         return view;
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog datePickerDialog, int year, int month, int day) {
+        butDate.setText(day + "/" + month + "/" + year);
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout radialPickerLayout, int hourOfDay, int minute) {
+        butTime.setText(hourOfDay + "H" + minute);
     }
 }
